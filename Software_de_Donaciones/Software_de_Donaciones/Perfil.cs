f4f01b4c1dd38c792842fe8e0a552ed3099cb934
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-09-29 20:28:10 -0300

    Nuevo Codigo
    Nuevo Codigo, se agregan la clase BaseDeDatos y el metodo para conseguir la info del Usuario en Usuario.cs

--- Software_de_Donaciones/Software_de_Donaciones/Perfil.cs
+++ Software_de_Donaciones/Software_de_Donaciones/Perfil.cs
@@ -3,8 +3,12 @@ namespace Software_de_Donaciones
 {
     public class Perfil
     {
+        private int ci = 0;
+
         public Perfil()
         {
         }
+
+        public int Ci { get => ci; set => ci = value; }
     }
 }
--- Software_de_Donaciones/Software_de_Donaciones/Usuario.cs
+++ Software_de_Donaciones/Software_de_Donaciones/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 namespace Software_de_Donaciones
 {
     public class Usuario
@@ -6,8 +7,22 @@ namespace Software_de_Donaciones
         //llamamos al perfil que el usuario va a tener.
         Perfil perfilDeUsuario = new Perfil();
 
+        private string nUsuario = "";
+        private string hash = "";
+        private string sal = "";
+
         public Usuario()
         {
         }
+
+        public Usuario(string nUsuario,string contraseña)
+        {
+            NUsuario = nUsuario;
+
+        }
+
+        public string NUsuario { get => nUsuario; set => nUsuario = value; }
+        public string Hash { get => hash; set => hash = value; }
+        public string Sal { get => sal; set => sal = value; }
     }
 }
