b15eeba0858e5075cf2f366bef567ca3bd424f55
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-09-29 20:04:55 -0300

    Nuevo Codigo

---
+++ Software_de_Donaciones/Software_de_Donaciones/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Software_de_Donaciones
+{
+    public class Usuario
+    {
+        //llamamos al perfil que el usuario va a tener.
+        Perfil perfilDeUsuario = new Perfil();
+
+        public Usuario()
+        {
+        }
+    }
+}
