e731c7fb99737206105214c4dca78a98eea5c6f8
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-09-29 21:16:29 -0300

    Codigo Nuevo
    Creamos las clases BaseDeDatos y UtilesXML
    Nos quedamos a medio camino de la configuración al linkeo de las credenciales del usuario por parte de la Base de Datos

--- Software_de_Donaciones/Software_de_Donaciones/BaseDeDatos.cs
+++ Software_de_Donaciones/Software_de_Donaciones/BaseDeDatos.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 namespace Software_de_Donaciones
 {
     public class BaseDeDatos
-    {
+    { 
+        
         public BaseDeDatos()
         {
         }
+
+        private MySqlConnection CrearConexion(string servidor, string bdd, string usuario, string passwd)
+        {
+            MySqlConnection conexion;
+            //Creamos el objeto de conexión
+
+            string datosConexion = "server=" + servidor + ";" +
+                                   "database=" + bdd + ";" +
+                                   "uid=" + usuario + ";" +
+                                   "pwd=" + passwd + ";";
+            //Declaro un string con los datos de conexión, agregando los parámetros
+
+            conexion = new MySqlConnection(datosConexion);
+            //Inicializo el objeto de conexión con los datos de conexión
+
+            return conexion;
+            //Devolvemos el objeto de conexión pronto
+        }
+
     }
 }
---
+++ Software_de_Donaciones/Software_de_Donaciones/Consultas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Software_de_Donaciones
+{
+    public class Consultas
+    {
+        private BaseDeDatos bdd = new BaseDeDatos();
+
+        public ObtenerDatosLogInLegales()
+        {
+
+        }
+
+        public Consultas()
+        {
+        }
+    }
+}
