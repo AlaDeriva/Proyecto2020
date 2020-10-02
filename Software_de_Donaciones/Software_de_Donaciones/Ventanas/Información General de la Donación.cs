3670466f2cb3938e4b60cad9028b7c3b15750367
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-10-02 20:58:30 -0300

    Informacion General de la Donacion

---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Información General de la Donación.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_de_Donaciones.Ventanas
+{
+    public partial class Información_General_de_la_Donación : Form
+    {
+        public Información_General_de_la_Donación()
+        {
+            InitializeComponent();
+        }
+    }
+}
