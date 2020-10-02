eb4ced9d3ae2aa9e6d8b2b3eadc67cca2460d3f2
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-10-02 20:10:21 -0300

    Agregar Nombre de la Categoria

---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Agregar Nombre de la Categoría.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_de_Donaciones.Ventanas
+{
+    public partial class Agregar_Nombre_de_la_Categoría : Form
+    {
+        public Agregar_Nombre_de_la_Categoría()
+        {
+            InitializeComponent();
+        }
+    }
+}
