db55dee52a4ab9b7c42ed3752399cb40c9d73921
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-10-02 19:42:59 -0300

    Ventana de Deposito

---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Ventana Deposito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_de_Donaciones.Ventanas
+{
+    public partial class Ventana_Deposito : Form
+    {
+        public Ventana_Deposito()
+        {
+            InitializeComponent();
+        }
+    }
+}
