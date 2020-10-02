407620183e85f99e8260e4b79824a94b748524e8
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-10-02 20:22:52 -0300

    Registro Actual del Material Previo a la Llegada del Deposito

---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Registro Actual del Material Previo a la Llegada del Deposito.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Software_de_Donaciones.Ventanas
+{
+    partial class Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<- Ir al Menú Principal";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(155, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "VER INFORMACIÓN DE LA DONACIÓN";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Registro Actual del Material Previo a la Llegada del Deposito";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(95, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(396, 108);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 272);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito";
+            this.Text = "Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Registro Actual del Material Previo a la Llegada del Deposito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_de_Donaciones.Ventanas
+{
+    public partial class Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito : Form
+    {
+        public Registro_Actual_del_Material_Previo_a_la_Llegada_del_Deposito()
+        {
+            InitializeComponent();
+        }
+    }
+}
