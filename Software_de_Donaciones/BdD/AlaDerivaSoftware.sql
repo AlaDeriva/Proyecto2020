9756f06742cb3ec5f4b9c493a90ae70581694b72
ShodytheWolf <49129728+ShodytheWolf@users.noreply.github.com>
2020-10-14 17:32:38 -0300

    Nuevos DIA y Tabla Relaciones
    Se actualizarón el DIA y la Tabla Relaciones

--- Software_de_Donaciones/BdD/AlaDerivaSoftware.sql
+++ Software_de_Donaciones/BdD/AlaDerivaSoftware.sql
@@ -22,6 +22,7 @@ Estado ENUM ("Abierta","Cerrada") NOT NULL default "Abierta",
 Fecha_Hora TIMESTAMP NOT NULL default current_timestamp);
 
 CREATE TABLE Beneficiario(
+ID INT (10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 CI INT (10) NOT NULL unique,
 Tipo ENUM ("Donante", "Destinatario") NOT NULL,
 Nombre VARCHAR (12) NOT NULL,
@@ -31,13 +32,13 @@ E-mail VARCHAR (12) NOT NULL unique,
 Direccion VARCHAR (12) NOT NULL unique);
 
 CREATE TABLE Deposito(
-ID INT (10) NOT NULL PRIMARY KEY,
+ID INT (10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Ubicacion VARCHAR (20) NOT NULL unique,
 Nombre VARCHAR (12) NOT NULL,
 Telefono VARCHAR (10) NOT NULL unique);
 
 CREATE TABLE Donacion(
-ID INT (10) NOT NULL PRIMARY KEY,
+ID INT (10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Cantidad INT (10) NOT NULL,
 Destino VARCHAR (20) NOT NULL unique,
 Fecha_Hora Envio TIMESTAMP NOT NULL default current_timestamp
@@ -52,7 +53,7 @@ ID_Sesion INT (10) NOT NULL PRIMARY KEY,
 ID_Usuario INT (10) NOT NULL);
 
 CREATE TABLE Supervisa(
-ID INT (10) NOT NULL PRIMARY KEY,
+ID INT (10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Donacion_ID INT (10) NOT NULL,
 Usuario_NombreUsuario VARCHAR (20) NOT NULL,
 Tipo VARCHAR (20) NOT NULL);
@@ -66,7 +67,9 @@ ID_Donacion INT (10) NOT NULL PRIMARY KEY,
 ID_Donante INT (10) NOT NULL);
 
 CREATE TABLE Manejo(
-ID_Usuario_ID_Deposito INT (10) NOT NULL unique);
+ID INT (10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ID_Usuario INT (10) NOT NULL,
+ID_Deposito INT (10) NOT NULL);
 
 CREATE TABLE Almacenada(
 ID_Donacion INT (10) NOT NULL PRIMARY KEY,
@@ -75,6 +78,9 @@ Direccion VARCHAR (20) NOT NULL,
 Numero INT (10) NOT NULL,
 Capcidad VARCHAR (20) NOT NULL,
 Condicion VARCHAR (20) NOT NULL);
+
+
+
 
 
 
