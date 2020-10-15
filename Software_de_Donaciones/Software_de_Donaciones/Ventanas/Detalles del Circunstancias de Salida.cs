89ea4fa8f73300d09dbe95da06989bc782639ff8
AlaDeriva <72109424+AlaDeriva@users.noreply.github.com>
2020-10-15 20:15:53 -0300

    Detalles de Salida del Material

---
+++ Software_de_Donaciones/Software_de_Donaciones/Ventanas/Detalles del Circunstancias de Salida.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_de_Donaciones.Ventanas
+{
+    public partial class Detalles_del_Circunstancias_de_Salida : Form
+    {
+        public Detalles_del_Circunstancias_de_Salida()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
