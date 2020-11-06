70ba18608ab4b3703bdda5f5b9c59f26a2f751b1
ShodytheWolf <49129728+ShodytheWolf@users.noreply.github.com>
2020-11-06 10:34:37 -0300

    Codigo Nuevo
    Se terminó de añadir el metodo para recibir la info para iniciar sesión.
    
    Se empezó el metodo para registrar usuarios.

--- Software_de_Donaciones/Software_de_Donaciones/Consultas.cs
+++ Software_de_Donaciones/Software_de_Donaciones/Consultas.cs
@@ -29,6 +29,8 @@ namespace Software_de_Donaciones
         //Variables para almacenar los datos de conexión con la base de datos
 
 
+
+        // Metodo para saber si el Usuario existe previamente en la BdD
         public bool UsuarioExiste(Usuario usuario)
         {
             bool Resultado = false;
@@ -71,15 +73,72 @@ namespace Software_de_Donaciones
                 Console.WriteLine("Error buscando Usuario: "+ex.Message);
 
             }
-
+            conexion.Close();
             return Resultado;
         }
 
-        public string TokensDeUsuario(Usuario usuario)
-        {
-            string ResultadoDeToken;
 
-             
+        //Metodo para recibir la info que verifique al usuario y lo deje iniciar sesión.
+        public string[] TokensDeUsuario(Usuario usuario)
+        {
+            string[] ResultadoDeToken = new string[2];
+            // Acá se guarda el resultado de la consulta a la BdD, para bien, o para mal :C
+
+
+            MySqlConnection conexion = bdd.CrearConexion(iPservidor, bddAUsar, usuarioBD, contraseniaBD);
+
+            string Consulta = "SELECT Hash,Sal FROM Usuario WHERE NombreUsuario = @NUsuario";
+
+
+            try
+            {
+                conexion.Open();
+
+                MySqlCommand comando = new MySqlCommand(Consulta, conexion);
+                //Creamos un comando con la consulta y la conexión.
+                // Este objeto es el que EJECUTA la consulta
+
+                comando.Parameters.AddWithValue("@NUsuario", usuario.NUsuario);
+                //Agregamos el dato al parámetro de la consulta
+
+                MySqlDataReader lector = comando.ExecuteReader();
+                //Creamos un objeto lector, para recibir los datos de la consulta
+
+                if (lector.HasRows)
+                //Chequeamos que el lector contenga algún registro
+                {
+                    while (lector.Read())
+                    //Recorremos los registros del lector con un bucle WHILE y el método '.Read()'
+                    {
+                        ResultadoDeToken[0] = lector.GetString(0);
+                        ResultadoDeToken[1] = lector.GetString(1);
+                        //El lector lee los 2 resultados que nos tira la BdD
+                    }
+                }
+                else
+                {
+                    ResultadoDeToken[0] = "Error";
+                    ResultadoDeToken[1] = "El usuario no tiene Hash ni Sal";
+                    //NI IDEA QUE PONER ACA, Preguntarle a Sebita.
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error recibiendo datos del Usuario: " + ex.Message);
+
+            }
+            conexion.Close();
+            return ResultadoDeToken;
+        }
+
+
+
+        public string RegistrarAlUsuario(string NombreUsuario,string Hash, string Sal)
+        {
+            string ResultadoDeRegistro = "";
+
+            string consulta = 
         }
 
     }
